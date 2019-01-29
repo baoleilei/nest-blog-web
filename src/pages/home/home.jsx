b1575d8f806524897422b5bdc8899f351f414555
baoleilei <baoleilei@baoleileideMacBook-Pro.local>
2019-01-29 23:05:21 +0800

    add ant-design

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './home.less';
+
+class Home extends Component {
+  static propTypes = {
+  }
+
+  state = {
+  }
+
+  render() {
+    
+    return (
+      <main className="home-container">
+    
+      </main>
+    );
+  }
+}
+
+export default Home
